831181ebd8e8d86993bc7e4900f095aeeb9b154b
mbarekh <hazem.mbarek@hotmail.com>
2017-06-07 17:53:31 +0200

    extention start

---
+++ src/drawComponent.ts
@@ -0,0 +1,149 @@
+
+import * as go from "gojs";
+import { Parser } from "parser";
+import { stateMachineColor } from "graphicColors";
+import { NodeDataArrayTemplate, LinkDataArrayTemplate } from "gojsTemplates";
+
+export class DrawComponent {
+
+    private $: any;
+    public diagram: go.Diagram;
+
+    constructor() {
+    }
+
+    draw(parser: Parser, divId: string): void {
+        this.$ = go.GraphObject.make;
+        this.diagram = this.createDiagram(divId);
+        this.diagram.nodeTemplate = this.getNodeTemplate();
+        this.diagram.nodeTemplateMap.add("LinkLabel", this.getLinkLabelTemplate());
+        this.diagram.groupTemplate = this.getGroupTemplate();
+        this.diagram.linkTemplate = this.getLinkTemplate();
+        this.diagram.model = this.getModel(parser.getNodeDataArray(), parser.getLinkDataArray());
+    }
+
+    private createDiagram(divId: string): go.Diagram {
+        const $ = this.$;
+        const thisObject = this;
+        const diagram =
+            $(go.Diagram, divId,
+                {
+                    contentAlignment: go.Spot.Center,
+                    InitialLayoutCompleted: (e) => { thisObject.loadControls(e.diagram); }
+                });
+        return diagram;
+    }
+
+    private loadControls(diagram): void {
+        diagram.links.each((link) => {
+            const arr = link.data.controls;
+            if (!Array.isArray(arr) || arr.length < 4) return;
+            const from = link.fromPort;
+            const to = link.toPort;
+            if (from === null || to === null) return;
+            let firstPoint, firstControlPoint, secondControlPoint, lastPoint;
+            if (arr.length === 4) {
+                firstControlPoint = new go.Point(arr[0], arr[1]);
+                secondControlPoint = new go.Point(arr[2], arr[3]);
+                firstPoint = link.getLinkPointFromPoint(from.part, from, from.getDocumentPoint(go.Spot.Center), firstControlPoint, true);
+                lastPoint = link.getLinkPointFromPoint(to.part, to, to.getDocumentPoint(go.Spot.Center), secondControlPoint, false);
+            } else if (arr.length === 6) {
+                firstControlPoint = new go.Point(arr[0], arr[1]);
+                secondControlPoint = new go.Point(arr[2], arr[3]);
+                lastPoint = new go.Point(arr[4], arr[5]);
+                firstPoint = link.getLinkPointFromPoint(from.part, from, from.getDocumentPoint(go.Spot.Center), firstControlPoint, true);
+            }
+            const list = new go.List();
+            list.add(firstPoint);
+            list.add(firstControlPoint);
+            list.add(secondControlPoint);
+            list.add(lastPoint);
+            link.points = list;
+        });
+    }
+
+    private getLinkLabelTemplate(): go.Part {
+        const $ = this.$;
+        const linkLabelTemplate = $(go.Node,
+            {
+                locationSpot: go.Spot.Center,  // Node.location is the center of the Shape
+                layerName: "Foreground"
+            },  // always have link label nodes in front of Links
+            $(go.TextBlock,                   // this is a Link label
+                new go.Binding("text", "text"))
+        );
+        return linkLabelTemplate;
+    }
+
+
+
+    private getNodeTemplate(): go.Part {
+        const $ = this.$;
+        const nodeTemplate =
+            $(go.Node, "Vertical",
+                {
+                    locationSpot: go.Spot.Center,  // Node.location is the center of the Shape
+                    locationObjectName: "SHAPE"
+                },
+                $(go.TextBlock, { stroke: "white", margin: 2 },
+                    new go.Binding("text"), new go.Binding("numberOfInstances")),
+                $(go.Shape, "Ellipse",
+                    {
+                        name: "SHAPE",
+                        strokeWidth: 2,
+                        desiredSize: new go.Size(30, 30),
+                        portId: ""
+                    },
+                    new go.Binding("fill", "fill"), new go.Binding("stroke", "stroke")
+                ),
+                new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify)
+            );
+        return nodeTemplate;
+    }
+
+    private getGroupTemplate(): go.Group {
+        const $ = this.$;
+        const groupTemplate =
+            $(go.Group, "Auto",
+                $(go.Shape, "Rectangle",
+                    { fill: stateMachineColor }),
+                $(go.Panel, "Vertical",
+                    {
+                        margin: 5,
+                        defaultAlignment: go.Spot.Center
+                    },
+                    $(go.TextBlock, { alignment: go.Spot.Center, font: "Bold 12pt Sans-Serif" },
+                        new go.Binding("text", "text")),
+                    $(go.Placeholder), { padding: 5 }
+                )
+            );
+        return groupTemplate;
+    }
+
+
+    private getLinkTemplate(): go.Link {
+        const $ = this.$;
+        const linkTemplate =
+            $(go.Link,
+                { curve: go.Link.Bezier, adjusting: go.Link.Stretch, reshapable: true },
+                $(go.Shape, new go.Binding("stroke", "strokeLink")),
+                $(go.Shape, { toArrow: "Standard" }, new go.Binding("fill", "fillArrow"), new go.Binding("stroke", "strokeArrow"))
+            );
+        return linkTemplate;
+    }
+
+
+    private getModel(nodeDataArray: Array<NodeDataArrayTemplate>, linkDataArray: Array<LinkDataArrayTemplate>): go.Model {
+        const data = {
+            "class": "go.GraphLinksModel",
+            "linkLabelKeysProperty": "labelKeys",
+            "nodeDataArray": nodeDataArray,
+            "linkDataArray": linkDataArray
+        };
+        return go.Model.fromJson(data);
+    }
+}
+
+
+
+
---
+++ src/parser.ts
@@ -0,0 +1,353 @@
+import { graphicalTags, modelTags } from "configurationParser";
+import { LinkLabelTemplate, TransitionTemplate, TriggerableTransitionTemplate, StateMachineTemplate, StateTemplate, LinkDataArrayTemplate, NodeDataArrayTemplate } from "gojsTemplates";
+import { Point, Curve, StateMachine, State, ComponentGraphicalModel } from "parserObjects";
+import { finalStateColor, stateColor, transitionPatternStateColor, entryPointStateColor } from "graphicColors";
+export class Parser {
+    private locations: { [key: string]: Point };
+    private controlPointTransition: { [key: string]: Curve };
+    private controlPointTriggerable: { [key: string]: Curve };
+    private stateMachines: { [key: string]: StateMachine };
+    private states: { [key: string]: State };
+    private transitionPatternStates: { [key: string]: State };
+    private linksLabel: Array<LinkLabelTemplate>;
+    private componentGraphicalModel: ComponentGraphicalModel;
+
+    private componentName: string;
+    private finalStates: Array<String>;
+    private entryPointState: string;
+    private entryPointStateMachine: string;
+    private stateMachineNames: Array<string>;
+    private linkDataArray: Array<LinkDataArrayTemplate>;
+    private nodeDataArray: Array<NodeDataArrayTemplate>;
+
+    constructor(componentGraphicalModel: ComponentGraphicalModel) {
+        this.componentGraphicalModel = componentGraphicalModel;
+    }
+
+    parse() {
+        this.parseGraphical();
+        this.parseModel();
+    }
+
+    private parseGraphical(): void {
+        this.locations = {};
+        const xmlGraphicalDom = (new DOMParser()).parseFromString(this.componentGraphicalModel.graphical, "text/xml");
+        let stateGraphicalData = xmlGraphicalDom.getElementsByTagName(graphicalTags.States)[0].getElementsByTagName(graphicalTags.StateGraphicalData);
+        for (let i = 0; i < stateGraphicalData.length; i++) {
+            const id = stateGraphicalData[i].getAttribute(graphicalTags.Id);
+            this.locations[id] = {
+                x: parseFloat(stateGraphicalData[i].getAttribute(graphicalTags.CenterX)),
+                y: parseFloat(stateGraphicalData[i].getAttribute(graphicalTags.CenterY))
+            };
+        }
+
+        stateGraphicalData = xmlGraphicalDom.getElementsByTagName(graphicalTags.TransitionPatternStates)[0].getElementsByTagName(graphicalTags.StateGraphicalData);
+        for (let i = 0; i < stateGraphicalData.length; i++) {
+            const id = stateGraphicalData[i].getAttribute(graphicalTags.Id);
+            this.locations[id] = {
+                x: parseFloat(stateGraphicalData[i].getAttribute(graphicalTags.CenterX)),
+                y: parseFloat(stateGraphicalData[i].getAttribute(graphicalTags.CenterY))
+            };
+        }
+
+        let transitionGraphicalData;
+
+        transitionGraphicalData = xmlGraphicalDom.getElementsByTagName(graphicalTags.Links)[0]
+            .getElementsByTagName(graphicalTags.TransitionGraphicalData);
+        this.controlPointTransition = this.getControlPointTransition(transitionGraphicalData);
+
+        transitionGraphicalData = xmlGraphicalDom.getElementsByTagName(graphicalTags.TransversalLinks)[0]
+            .getElementsByTagName(graphicalTags.TransitionGraphicalData);
+        this.controlPointTriggerable = this.getControlPointTransition(transitionGraphicalData);
+    }
+
+    private parseModel(): void {
+        const scxmlDom = (new DOMParser()).parseFromString(this.componentGraphicalModel.model, "text/xml");
+        this.setComponentName(scxmlDom);
+        this.setStateMachines(scxmlDom);
+        this.setStates(scxmlDom);
+        this.setLinks(scxmlDom);
+        this.finalStates = this.setFinalStates();
+        this.entryPointState = this.getEntyPointState();
+        this.nodeDataArray = this.setNodeDataArray();
+        this.nodeDataArray = this.nodeDataArray.concat(this.linksLabel);
+        this.addControlPoint();
+    }
+
+    private setComponentName(scxmlDom: Document): void {
+        this.componentName = scxmlDom.getElementsByTagName(modelTags.ComponentViewModelData)[0].getAttribute(modelTags.Name);
+    }
+
+    private addControlPoint(): void {
+        for (let i = 0; i < this.linkDataArray.length; i++) {
+            if (this.linkDataArray[i].triggerable) {
+                const keyLink = this.linkDataArray[i].key;
+                const firstPoint = this.controlPointTriggerable[keyLink].firstPoint;
+                const lastPoint = this.controlPointTriggerable[keyLink].lastPoint;
+                const firstControlPoint = this.controlPointTriggerable[keyLink].firstControlPoint;
+                const secondControlPoint = this.controlPointTriggerable[keyLink].secondControlPoint;
+                this.linkDataArray[i].controls = [firstControlPoint.x, firstControlPoint.y, secondControlPoint.x, secondControlPoint.y, lastPoint.x, lastPoint.y];
+            } else {
+                const keyLink = this.linkDataArray[i].key;
+                const firstPoint = this.controlPointTransition[keyLink].firstPoint;
+                const lastPoint = this.controlPointTransition[keyLink].lastPoint;
+                const firstControlPoint = this.controlPointTransition[keyLink].firstControlPoint;
+                const secondControlPoint = this.controlPointTransition[keyLink].secondControlPoint;
+                this.linkDataArray[i].controls = [firstControlPoint.x, firstControlPoint.y, secondControlPoint.x, secondControlPoint.y];
+            }
+        }
+    }
+
+    private setNodeDataArray(): Array<NodeDataArrayTemplate> {
+        const nodeDataArray: Array<NodeDataArrayTemplate> = [];
+        let ids, state, stateMachine, id;
+        ids = Object.keys(this.stateMachines);
+        for (let i = 0; i < ids.length; i++) {
+            stateMachine = this.stateMachines[ids[i]];
+            nodeDataArray.push({
+                "key": stateMachine.name,
+                "text": stateMachine.name,
+                "isGroup": true,
+                "numberOfInstances": 0
+            });
+        }
+        ids = Object.keys(this.states).concat(Object.keys(this.transitionPatternStates));
+        for (let j = 0; j < ids.length; j++) {
+            state = this.states[ids[j]];
+            let loc, color, text;
+            if (!this.states[ids[j]]) {
+                id = ids[j].substring(modelTags.TP_State.length, ids[j].length);
+                loc = this.locations[id].x + " " + this.locations[id].y;
+                color = transitionPatternStateColor;
+                state = this.transitionPatternStates[ids[j]];
+                text = state.name;
+            } else {
+                id = ids[j].substring(modelTags.State.length, ids[j].length);
+                loc = this.locations[id].x + " " + this.locations[id].y;
+                text = state.name + "";
+                if (state.isFinal) {
+                    color = finalStateColor;
+                } else if (!state.isEntryPoint) {
+                    color = stateColor;
+                } else {
+                    color = entryPointStateColor;
+                }
+            }
+            nodeDataArray.push({
+                "key": state.key,
+                "text": text,
+                "group": state.group,
+                "stateName": state.name,
+                "numberOfStates": 0,
+                "fill": color,
+                "stroke": color,
+                "loc": loc
+            });
+        }
+        return nodeDataArray;
+    }
+
+    private getControlPointTransition(transitionGraphicalData: NodeListOf<Element>): { [key: string]: Curve } {
+        const controlPoints: { [key: string]: Curve } = {};
+        for (let i = 0; i < transitionGraphicalData.length; i++) {
+            const id = transitionGraphicalData[i].getAttribute(graphicalTags.Id);
+            const points = transitionGraphicalData[i].getElementsByTagName(graphicalTags.Point);
+            controlPoints[id] = {
+                firstPoint: {
+                    x: parseFloat(points[0].getElementsByTagName(graphicalTags.X)[0].innerHTML),
+                    y: parseFloat(points[0].getElementsByTagName(graphicalTags.Y)[0].innerHTML)
+                },
+                firstControlPoint: {
+                    x: parseFloat(points[1].getElementsByTagName(graphicalTags.X)[0].innerHTML),
+                    y: parseFloat(points[1].getElementsByTagName(graphicalTags.Y)[0].innerHTML)
+                },
+                secondControlPoint: {
+                    x: parseFloat(points[2].getElementsByTagName(graphicalTags.X)[0].innerHTML),
+                    y: parseFloat(points[2].getElementsByTagName(graphicalTags.Y)[0].innerHTML)
+                },
+                lastPoint: {
+                    x: parseFloat(points[3].getElementsByTagName(graphicalTags.X)[0].innerHTML),
+                    y: parseFloat(points[3].getElementsByTagName(graphicalTags.Y)[0].innerHTML)
+                }
+            };
+        }
+        return controlPoints;
+    }
+
+    private getLocation(id: string): string {
+        id = id.substring(modelTags.State.length, id.length);
+        return this.locations[id].x + " " + this.locations[id].y;
+    }
+
+    private setFinalStates(): Array<String> {
+        const finalStates = [];
+        for (let id in this.states) {
+            if (this.states[id].isFinal) {
+                finalStates.push(this.states[id].key);
+            }
+        }
+        return finalStates;
+    }
+
+    private getEntyPointState(): string {
+        for (let id in this.states) {
+            if (this.states[id].isEntryPoint) {
+                return this.states[id].key;
+            }
+        }
+    }
+
+    private setLinks(scxmlDom: Document): void {
+        const linksDom = scxmlDom.getElementsByTagName(modelTags.TransitionData);
+        this.linkDataArray = [];
+        this.linksLabel = [];
+        let key,
+            from,
+            to,
+            text;
+        for (let i = 0; i < linksDom.length; i++) {
+            from = this.states[linksDom[i].getAttribute(modelTags.FromKey)];
+            if (!from) {
+                from = this.transitionPatternStates[linksDom[i].getAttribute(modelTags.FromKey)];
+                for (let k = 0; k < from.selectedStatesKeys.length; k++) {
+                    this.states[from.selectedStatesKeys[k]].isFinal = false;
+                }
+            }
+            from.isFinal = false;
+            to = this.states[linksDom[i].getAttribute(modelTags.ToKey)];
+            text = linksDom[i].getAttribute(modelTags.Name);
+            key = linksDom[i].getAttribute(modelTags.Id);
+            this.linkDataArray.push({
+                "key": key,
+                "from": from.key,
+                "stateMachineTarget": from.group,
+                "to": to.key,
+                "text": text,
+                "messageType": linksDom[i].getAttribute(modelTags.TriggeringEvent),
+                "labelKeys": [key],
+                "triggerable": false,
+                "controls": null
+            });
+            this.linksLabel.push({
+                "key": key,
+                "category": "LinkLabel",
+                "text": text
+            });
+        }
+
+        const triggerableLinksDom = scxmlDom.getElementsByTagName(modelTags.TransversalTransitionData);
+        for (let j = 0; j < triggerableLinksDom.length; j++) {
+            from = this.states[triggerableLinksDom[j].getAttribute(modelTags.FromKey)];
+            if (!from)
+                from = this.transitionPatternStates[triggerableLinksDom[j].getAttribute(modelTags.FromKey)];
+            this.linkDataArray.push({
+                "key": triggerableLinksDom[j].getAttribute(modelTags.Id),
+                "from": from.key,
+                "to": triggerableLinksDom[j].getAttribute(modelTags.ToId),
+                "strokeLink": "red",
+                "strokeArrow": "red",
+                "fillArrow": "red",
+                "triggerable": true,
+                "controls": null
+            });
+        }
+    }
+
+    private setStates(scxmlDom: Document): void {
+        const statesDom = scxmlDom.getElementsByTagName(modelTags.StateData);
+        const states = {};
+        let id,
+            group,
+            name,
+            isEntryPoint;
+        let entryPointStateMachine;
+        for (let i = 0; i < statesDom.length; i++) {
+            id = statesDom[i].getAttribute(modelTags.SubGraphKey);
+            group = this.stateMachines[id].name;
+            name = statesDom[i].getAttribute(modelTags.Name);
+            isEntryPoint = statesDom[i].getAttribute(modelTags.IsEntryPoint) === "true";
+            states[modelTags.State + statesDom[i].getAttribute(modelTags.Id)] = {
+                name: name,
+                group: group,
+                key: group + modelTags.Separator + name,
+                isFinal: true,
+                isEntryPoint: isEntryPoint
+            };
+            if (isEntryPoint) {
+                entryPointStateMachine = group;
+            }
+
+        }
+        // transition pattern state
+        const transitionPatternStates = {};
+        const transitionPatternStateDataDom = scxmlDom.getElementsByTagName(modelTags.TransitionPatternStateData);
+        for (let i = 0; i < transitionPatternStateDataDom.length; i++) {
+            id = transitionPatternStateDataDom[i].getAttribute(modelTags.SubGraphKey);
+            group = this.stateMachines[id].name;
+            name = transitionPatternStateDataDom[i].getAttribute(modelTags.Name);
+            const selectedStatesKeys = [];
+            const selectedStatesKeysDom = transitionPatternStateDataDom[i].getElementsByTagName(modelTags.SelectedStatesKeys)[0]
+                .getElementsByTagName(modelTags.string);
+            for (let k = 0; k < selectedStatesKeysDom.length; k++) {
+                selectedStatesKeys.push(selectedStatesKeysDom[k].innerHTML);
+            }
+            transitionPatternStates[modelTags.TP_State + transitionPatternStateDataDom[i].getAttribute(modelTags.Id)] = {
+                name: name,
+                group: group,
+                key: group + modelTags.Separator + name,
+                isFinal: true,
+                isPatternTransitionState: true,
+                selectedStatesKeys: selectedStatesKeys
+            };
+        }
+        this.states = states;
+        this.transitionPatternStates = transitionPatternStates;
+        this.entryPointStateMachine = entryPointStateMachine;
+    }
+
+    private setStateMachines(scxmlDom: Document): void {
+        const stateMachineDom = scxmlDom.getElementsByTagName(modelTags.StateMachineData);
+        const stateMachines = {};
+        const stateMachineNames = [];
+        let id, name;
+        for (let i = 0; i < stateMachineDom.length; i++) {
+            id = stateMachineDom[i].getAttribute(modelTags.Id);
+            name = stateMachineDom[i].getAttribute(modelTags.Name);
+            stateMachines[modelTags.StateMachine + id] = {
+                name: name,
+                publicMember: stateMachineDom[i].getAttribute(modelTags.PublicMember)
+            };
+            stateMachineNames.push(name);
+        }
+        this.stateMachines = stateMachines;
+        this.stateMachineNames = stateMachineNames;
+    }
+
+    public getFinalStates(): Array<String> {
+        return this.finalStates;
+    };
+
+    public getEntryPointState(): string {
+        return this.entryPointState;
+    };
+
+    public getEntryPointStateMachine(): string {
+        return this.entryPointStateMachine;
+    };
+
+    public getStateMachineNames(): Array<string> {
+        return this.stateMachineNames;
+    };
+
+    public getLinkDataArray(): Array<LinkDataArrayTemplate> {
+        return this.linkDataArray;
+    };
+
+    public getNodeDataArray(): Array<NodeDataArrayTemplate> {
+        return this.nodeDataArray;
+    };
+
+    public getComponentName(): string {
+        return this.componentName;
+    };
+}
