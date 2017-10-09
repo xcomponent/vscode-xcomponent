06c09d89e2124dc01a6a6de92307e6afb73d06cf
nicolaserny <nicolas.erny@xcomponent.com>
2017-10-09 13:52:42 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 version=`git tag -l --points-at HEAD`
 echo $version
 
-if [[ ! -z $version ]]; then
+if [ ! -z "$version" ]; then
 	echo "Deploying..."
     cd extension && vsce publish -p $1
 else
