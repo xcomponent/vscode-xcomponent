7320398641a1114be03ce7fb6ec8a561597a26f7
nicolaserny <nicolas.erny@xcomponent.com>
2017-10-10 11:25:36 +0200

    Fix previews inde vsix package + package upgrades

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ echo $version
 
 if [ ! -z "$version" ]; then
 	echo "Deploying..."
-    cd extension && yarn install && vsce publish -p $1
+    cd extension && vsce publish -p $1
 else
 	echo "Nothing to deploy - not a release version"
+	echo "Testing the package command"
+	cd extension && vsce package
 fi
