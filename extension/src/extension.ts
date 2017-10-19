99b0d696e626ea32cf005c506a07392ad7f88b9e
mbarekh <hazem.mbarek@hotmail.com>
2017-10-19 15:05:10 +0200

    add xcbuild path and mono path to vscode settings

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -67,16 +67,33 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     const disposableBuild = vscode.commands.registerCommand("xcomponent.build.project", () => {
+        const xcbuildPath = vscode.workspace.getConfiguration()["xcbuild"];
+        if (!fs.existsSync(xcbuildPath)) {
+            vscode.window.showErrorMessage(`xcbuild.exe not found. Please specify xcbuild path in vscode settings`);
+            return;
+        }
         const rootPath = vscode.workspace.rootPath;
         const baseName = path.basename(rootPath);
         const xcmlPath = `${rootPath}${path.sep}${baseName}_Model.xcml`;
-        const buildCommand = `xcbuild --compilationmode=Debug --build --env=Dev --vs=VS2015 --project=${xcmlPath}`;
-        if (fs.existsSync(xcmlPath)) {
+        if (!fs.existsSync(xcmlPath)) {
+            vscode.window.showErrorMessage(`Build error. File ${xcmlPath} not found`);
+            return;
+        }
+        const isWindowsPlatform = /^win/.test(process.platform);
+        if (isWindowsPlatform) {
+            const buildCommand = `${xcbuildPath} --compilationmode=Debug --build --env=Dev --vs=VS2015 --project=${xcmlPath}`;
             terminal.show();
             terminal.sendText(buildCommand);
-        } else {
-            vscode.window.showErrorMessage(`Build error. File ${xcmlPath} not found`);
+            return;
         }
+        const monoPath = vscode.workspace.getConfiguration()["mono"];
+        if (!fs.existsSync(monoPath)) {
+            vscode.window.showErrorMessage(`mono not found. Please specify mono path in vscode settings`);
+            return;
+        }
+        const buildCommand = `mono ${xcbuildPath}--compilationmode=Release --build --framework=Framework452 --env=Dev --vs=VS2015 --monoPath=“${monoPath}” --project=“${xcmlPath}”`
+        terminal.show();
+        terminal.sendText(buildCommand);
     });
 
 
