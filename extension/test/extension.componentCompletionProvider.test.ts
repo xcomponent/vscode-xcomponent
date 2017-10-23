1095afc15dfdf3c1090aa3f351f5361f90ade911
nicolaserny <nicolas.erny@xcomponent.com>
2017-10-23 14:46:48 +0200

    Add unit tests

--- extension/test/extension.componentCompletionProvider.test.ts
+++ extension/test/extension.componentCompletionProvider.test.ts
@@ -82,4 +82,55 @@ describe("ComponentCompletionItemProvider Tests", () => {
                 should.fail(reason, undefined);
             });
     });
+
+    it(`should provide transition name suggestion when autocomplete is triggered on TransitionName (TriggeredMethodData attribute)`, () => {
+        return vscode.workspace.openTextDocument(path.join(inputPath, cxmlFileName)).then(document => {
+            return vscode.window.showTextDocument(document);
+        })
+            .then(editor => {
+                const completionProvider = new ComponentCompletionItemProvider();
+                return completionProvider.provideCompletionItems(editor.document, new vscode.Position(16, 89), null);
+            }).then(completionItems => {
+                completionItems.length.should.eql(13);
+                completionItems[0].label.should.eql("ChooseTestTech");
+            }, reason => {
+                console.error(reason);
+                should.fail(reason, undefined);
+            });
+    });
+
+    it(`should provide triggering event suggestion when autocomplete is triggered on TriggeringEvent (TriggeredMethodData attribute)`, () => {
+        return vscode.workspace.openTextDocument(path.join(inputPath, cxmlFileName)).then(document => {
+            return vscode.window.showTextDocument(document);
+        })
+            .then(editor => {
+                const completionProvider = new ComponentCompletionItemProvider();
+                return completionProvider.provideCompletionItems(editor.document, new vscode.Position(16, 129), null);
+            }).then(completionItems => {
+                completionItems.length.should.eql(10);
+                completionItems[0].label.should.eql("XComponent.TechTest.UserObject.InputData");
+            }, reason => {
+                console.error(reason);
+                should.fail(reason, undefined);
+            });
+    });
+
+    it(`should provide TriggeredMethodData property suggestion when autocomplete is triggered in a TriggeredMethodData tag`, () => {
+        return vscode.workspace.openTextDocument(path.join(inputPath, cxmlFileName)).then(document => {
+            return vscode.window.showTextDocument(document);
+        })
+            .then(editor => {
+                const completionProvider = new ComponentCompletionItemProvider();
+                return completionProvider.provideCompletionItems(editor.document, new vscode.Position(16, 112), null);
+            }).then(completionItems => {
+                completionItems.length.should.eql(4);
+                completionItems[0].label.should.eql("IsNodeInitializer");
+                completionItems[1].label.should.eql("IsSelected");
+                completionItems[2].label.should.eql("TransitionName");
+                completionItems[3].label.should.eql("TriggeringEvent");
+            }, reason => {
+                console.error(reason);
+                should.fail(reason, undefined);
+            });
+    });
 });
