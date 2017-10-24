215cebf0194f5f5202fbc023a1a377e7ee5ba5b4
mbarekh <hazem.mbarek@hotmail.com>
2017-10-24 17:38:43 +0200

    fix unsecure option of bridge

--- extension/src/bridge.ts
+++ extension/src/bridge.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
+import { isSecure } from "./webSocketConfigParser";
 
 export const launchBridge = (terminal: vscode.Terminal): void => {
     const rootPath = vscode.workspace.rootPath;
@@ -9,7 +10,10 @@ export const launchBridge = (terminal: vscode.Terminal): void => {
     const isWindowsPlatform = /^win/.test(process.platform);
     const mono = (isWindowsPlatform) ? "" : "mono";
     const bridge = (fs.existsSync(config.xcwebsocketbridge.path)) ? config.xcwebsocketbridge.path : "XCWebSocketBridge.exe";
-    terminal.show();
-    const launchRuntimeCommand = `${mono} ${bridge} --unsecure --apiPath="${rootPath}${path.sep}output${path.sep}xcassemblies" --port=${config.xcwebsocketbridge.port}`;
+    const baseName = path.basename(rootPath);
+    const configurationFilePath = `${rootPath}${path.sep}Configuration.${baseName}${path.sep}Dev${path.sep}${baseName}_Deployment_Configuration.xml`;
+    const unsecure = isSecure(configurationFilePath) ? "" : "--unsecure";
+    const launchRuntimeCommand = `${mono} ${bridge} --apiPath="${rootPath}${path.sep}output${path.sep}xcassemblies" --port=${config.xcwebsocketbridge.port} ${unsecure}`;
+    terminal.show();    
     terminal.sendText(launchRuntimeCommand);
 };
